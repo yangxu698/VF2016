f2cd2d7b81a884ef1c7aed1da8617ffa20ce8e29
Yang <yangxu698@gmail.com>
2021-08-25 15:51:56 -0400

    debug

--- Code/tm_edi_local.sh
+++ Code/tm_edi_local.sh
@@ -6,5 +6,7 @@
 #$ -N  editorial_local # Specify job name
 
 module load R gsl
+export LD_LIBRARY_PATH=/afs/crc.nd.edu/user/y/yxu6/mpfr/lib:$LD_LIBRARY_PATH
+
 
 R CMD BATCH topicmodel_editorial_local.r
